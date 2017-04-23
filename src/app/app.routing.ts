119d48dd873c4f032ad1447631228dbba7f9660c
Cory Ginsberg <cory@coryginsberg.com>
2017-04-23 18:43:05 -0500

    Updated navbar and added it to the MyAccount page. Updated navbar 'account' dropdown to no longer have the settings button and added color to the logout button, as well as a dividing line. Also added the navbar to the new My Account page with the 'My Account' button on the navbar dropdown disabled.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,12 +6,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AccountComponent } from './account/account.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'account', component: AccountComponent },
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
