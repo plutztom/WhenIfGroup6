eed129f4a7c8e4123b3648ca7bc123be3c28f6b3
Cory Ginsberg <cory@coryginsberg.com>
2017-04-25 17:54:07 -0500

    Refactored and restructured most of the code.

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -1,11 +1,10 @@
-/**
- * Created by coryg on 4/22/2017.
- */
 export class User {
-  id: number;
-  username: string;
-  password: string;
-  fullName: string;
-  depaulID: number;
-  isAdmin = false;
+    id: number;
+
+    fullName: string;
+    depaulID: number;
+    email: string;
+    isAdmin = false;
+    username: string;
+    password: string;
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,35 +4,35 @@ import {User} from '../_models/user';
 import {UserService} from '../_services/user.service';
 
 @Component({
-  moduleId: module.id,
-  templateUrl: 'home.component.html'
+    moduleId: module.id,
+    templateUrl: 'home.component.html'
 })
 
 export class HomeComponent implements OnInit {
-  currentUser: User;
-  users: User[] = [];
-  isAdmin: boolean;
+    currentUser: User;
+    users: User[] = [];
+    isAdmin: boolean;
 
-  constructor(private userService: UserService) {
-    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    this.isAdmin = JSON.parse(localStorage.getItem(''));
-  }
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        this.isAdmin = JSON.parse(localStorage.getItem(''));
+    }
 
-  ngOnInit() {
-    this.loadAllUsers();
-  }
+    ngOnInit() {
+        this.loadAllUsers();
+    }
 
-  showAdmin() {
-    return this.isAdmin;
-  }
+    showAdmin() {
+        return this.isAdmin;
+    }
 
-  // deleteUser(id: number) {
-  //    this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
-  // }
+    // deleteUser(id: number) {
+    //    this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+    // }
 
-  private loadAllUsers() {
-    this.userService.getAll().subscribe(users => {
-      this.users = users;
-    });
-  }
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => {
+            this.users = users;
+        });
+    }
 }
